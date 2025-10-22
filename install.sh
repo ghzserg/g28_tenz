db418c966f0277edc954192eb87a69354d189961
Sergei Rozhkov <fgup@zserg.ru>
2025-10-22 18:32:50 +0500

    Zstop pin

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ awk '
   { print }
 ' ${CONF} > /tmp/tmp && mv /tmp/tmp $CONF && echo "Disable position_endstop in [stepper_z] printer.base.cfg"
 
-PIN="PD2"
-[ ${FF5X} -eq 0 ] && PIN="PB15"
+PIN="!PD2"
+[ ${FF5X} -eq 0 ] && PIN="!PB15"
 
 echo "[gcode_button _zstop]
 pin: $PIN
