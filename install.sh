ba6997af9dbab6c085bd0db1d7740adad9218f51
Sergei Rozhkov <fgup@zserg.ru>
2025-10-22 17:42:02 +0500

    Install

--- install.sh
+++ install.sh
@@ -3,15 +3,21 @@
 CONF=/opt/config/printer.base.cfg
 
 awk '
-/^\[stepper_z\]/, /^\[/ {
-  if (/^\[stepper_z\]/) { in_z=1; print; next }
-  if (/^\[/) { in_z=0; print; next }
-  if (in_z && /^[^;[:space:]].*position_endstop[[:space:]]*:/) {
+  /^\[stepper_z\]/ {
+    in_z = 1
+    print
+    next
+  }
+  /^\[/ {
+    in_z = 0
+    print
+    next
+  }
+  in_z && /^[[:space:]]*position_endstop[[:space:]]*:/ && !/^[[:space:]]*;/ {
     print ";" $0
     next
   }
-}
-{ print }
+  { print }
 ' ${CONF} > /tmp/tmp && mv /tmp/tmp $CONF && echo "Disable position_endstop in [stepper_z] printer.base.cfg"
 
 echo "G28_tenz installed"
--- uninstall.sh
+++ uninstall.sh
@@ -4,22 +4,31 @@ CONF=/opt/config/printer.base.cfg
 
 awk '
 /^\[stepper_z\]/ {
-  in_z = 1
-  print
-  next
+    in_z = 1
+    print
+    next
 }
 /^\[/ && in_z {
-  print "position_endstop: 220"
-  in_z = 0
+    in_z = 0
+    print
+    next
 }
-/^\[/ && !in_z { print; next }
-in_z {
-  if (/^[[:space:]]*;?[[:space:]]*position_endstop[[:space:]]*:/) next
-  print
-  next
+/^\[/ && !in_z {
+    print
+    next
 }
-!in_z { print }
-END { if (in_z) print "position_endstop: 220" }
-' ${CONF} > /tmp/tmp && mv /tmp/tmp $CONF && echo "Enable position_endstop in [stepper_z] printer.base.cfg"
+in_z && /position_endstop[[:space:]]*:/ {
+    next
+}
+{ print }
+' ${CONF} > tmp.cfg && \
+awk '
+/^\[stepper_z\]/ {
+    print
+    print "position_endstop: 220"
+    next
+}
+{ print }
+' /tmp/tmp > ${CONF} && rm /tmp/tmp && echo "Enable position_endstop in [stepper_z] printer.base.cfg"
 
 echo "G28_tenz uninstalled"
