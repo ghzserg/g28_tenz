73087619b764c597da66aff49d8fdeed9ea61d67
Sergei Rozhkov <zmod@zmod.link>
2026-05-08 10:21:14 +0500

    Update

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 
 source /opt/config/mod/.shell/0.sh
 
-PIN="!PD2"
-[ ${AD5X} -eq 0 ] && PIN="!PB15"
+PIN="!PB15"
+[ ${AD5X} -eq 1 ] && PIN="!PD2"
+[ ${AD5M} -eq 1 ] && PIN="!PB15"
 
 echo "[gcode_button _zstop]
 pin: $PIN
